ea5555a96b28d0cd3fd4fc5acd1928c5d4a3876d
Julian Meyer <jestry.privat@outlook.de>
2024-05-25 02:07:08 +0200

    addex pragma once, wrote little test in main

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,4 +1,11 @@
+#include "board.hpp" 
+#include <iostream>
 
 int main() {
-    
+    TicTacToe::Board board;
+    board.makeMove(0, 0, TicTacToe::Player::X);
+    board.makeMove(0, 1, TicTacToe::Player::X);
+    board.makeMove(0, 2, TicTacToe::Player::X);
+    board.print();
+    std::cout << "Did X win?" << (board.checkWin() == TicTacToe::Player::X ? "yes" : "no");
 }
--- include/player.hpp
+++ include/player.hpp
@@ -1,4 +1,4 @@
-
+#pragma once
 namespace TicTacToe {
     enum class Player { None, X, O };
 }
